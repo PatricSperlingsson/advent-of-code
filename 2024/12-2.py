8bcd9e010e217f40b79f3970c1741699d2f85a61
Patric Sperlingsson <patricsn@axis.com>
2024-12-12 16:41:22 +0100

    12-2 still not done
    Change-Id: I20a1df978a60f05463d07937bcc203429cceae61

--- 2024/12-2.py
+++ 2024/12-2.py
@@ -16,32 +16,59 @@ rows = len(grid)
 cols = len(grid[0])
 print(f"rows x cols: {rows} x {cols}")
 
-# Directions  Left,    Right,  Up,      Down
+# Directions  Up,      Down,   Left,    Right
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 # Initialize visited
 visited = [[False for _ in range(cols)] for _ in range(rows)]
 
-def bfs(x, y, plant):
-    start = (x, y)
+def bfs(r, c, plant):
+    start = (r, c, None, None)
     queue = deque([start])
-    visited[x][y] = True
+    visited[r][c] = True
     area = 0
-    perimeter = 0
+    region = set()
     different_sides = 0
     
     while queue:
-        x, y = queue.popleft()
+        r_prev, c_prev, dr_prev, dc_prev = queue.popleft()
         area += 1
         
-        for dx, dy in directions:
-            nx, ny = x + dx, y + dy
+        for dr, dc in directions:
+            nr, nc = r_prev + dr, c_prev + dc
+            print(f"  r_prev({r_prev}), dr({dr}), nr({nr})       c_prev({c_prev}), dc({dc}), nc({nc})")
             
-            if nx < 0 or ny < 0 or nx >= rows or ny >= cols or grid[nx][ny] != plant:
-                perimeter += 1
-            elif not visited[nx][ny]:
-                visited[nx][ny] = True
-                queue.append((nx, ny))
+            if nr < 0 or nc < 0 or nr >= rows or nc >= cols or grid[nr][nc] != plant:
+                print(f"    Check discount")
+                # Check if there is a discount
+                if dr_prev == None or dc_prev == None:
+                    different_sides += 1
+                    print(f"    Added sides: ({dr_prev},{dc_prev} => {nr},{nc})")
+                else:
+                    # Left, Right
+                    if (dr_prev, dc_prev) == (0, -1) or (dr_prev, dc_prev) == (0, 1):
+                        # Up from r_prev, c_prev
+                        nrUp, ncUp = r_prev - 1, c_prev + 0
+                        print(f"    nrUp{nrUp}, ncUp{ncUp}")
+                        if (nrUp, ncUp) not in region:
+                            # Down from r_prev, c_prev
+                            nrDown, ncDown = r_prev + 1, c_prev + 0
+                            if (nrDown, ncDown) not in region:
+                                different_sides += 1
+                                print(f"    Added sides: ({dr_prev},{dc_prev} => {nr},{nc})")
+
+                    # print(f"    nr1, nc1: {nr1}, {nc1}")
+                    # if 0 <= nr1 > rows and 0 <= nc1 > cols and (nr1, nc1) not in region:
+                    #     nr2, nc2 = r_prev - 2*dr, c_prev - 2*dc
+                    #     if 0 <= nr2 > rows and 0 <= nc2 > cols and (nr2, nc2) not in region:
+                    #         different_sides += 1
+                    
+            elif not visited[nr][nc]:
+                print(f"    {grid[nr][nc]} => Added region: ({nr}, {nc})")
+                visited[nr][nc] = True
+                region.add((nr, nc))
+                queue.append((nr, nc, dr, dc))
+        print(f"\n")
 
     return area * different_sides
 
@@ -54,4 +81,4 @@ for row in range(rows):
             fence_cost = bfs(row, col, plant)
             total_fence_cost += fence_cost
 print(f"Total Fence Cost: {total_fence_cost}")
-# Correct answer: 1433460
+# Correct answer: 50308 too low
