e9779becffc2832e327e8a694059edf0dd05e7df
PatricSperlingsson <oa_cantona@hotmail.com>
2024-12-17 07:43:10 +0100

    aoc24: 17-2 not done
    Change-Id: I0f670d8681f2fa227a7931e39bd0aa20eb3cb53b

---
+++ 2024/17-1.py
@@ -0,0 +1,101 @@
+#https://adventofcode.com/2024/day/17
+import numpy as np
+from collections import deque, Counter, defaultdict
+# Priority Queue
+from heapq import heappop, heappush
+import copy
+import sympy
+# Recursive function with memorization
+from functools import lru_cache
+#@lru_cache(None)
+# Creating functions
+from math import gcd
+from sympy import symbols, Eq, solve
+
+# Open and read the file as a single string
+with open('17i.txt', 'r') as file:
+    lines = [line.strip() for line in file.readlines()]
+print(f"lines: {lines}")
+
+# Parse input
+reg_a = int(lines[0].split(': ')[1])
+reg_b = int(lines[1].split(': ')[1])
+reg_c = int(lines[2].split(': ')[1])
+prog = [int(instr) for instr in lines[4].split(': ')[1].split(',')]
+prog_len = len(prog)
+print(f"reg_a: {reg_a}")
+print(f"reg_b: {reg_b}")
+print(f"reg_c: {reg_c}")
+print(f"prog: {prog}")
+print(f"prog_len: {prog_len}")
+
+instr_ptr = 0
+output = []
+while instr_ptr < prog_len:
+    opcode = prog[instr_ptr]
+    operand = prog[instr_ptr + 1]
+    instr_ptr += 2
+
+    if opcode == 0: # adv
+        print(f"adv: reg_a += reg_a // (2 ** operand)")
+        if operand < 4:
+            reg_a //= 2 ** operand
+        elif operand == 4:
+            reg_a //= 2 ** reg_a
+        elif operand == 5:
+            reg_a //= 2 ** reg_b
+        elif operand == 6:
+            reg_a //= 2 ** reg_c
+    elif opcode == 1: # bxl
+        print(f"bxl: reg_b += reg_b ^ operand")
+        reg_b ^= operand
+    elif opcode == 2: # bst
+        print(f"bst: reg_b = operand % 8")
+        if operand < 4:
+            reg_b = operand % 8
+        elif operand == 4:
+            reg_b = reg_a % 8
+        elif operand == 5:
+            reg_b = reg_b % 8
+        elif operand == 6:
+            reg_b = reg_c % 8
+    elif opcode == 3: # jnz
+        print(f"jnz: if reg_a != 0, jump to operand")
+        if reg_a != 0:
+            instr_ptr = operand
+    elif opcode == 4: # bxc
+        print(f"bxc: reg_b = reg_b ^ reg_c")
+        reg_b ^= reg_c
+    elif opcode == 5: # out
+        print(f"out: output = operand % 8")
+        if operand < 4:
+            output.append(operand % 8)
+        elif operand == 4:
+            output.append(reg_a % 8)
+        elif operand == 5:
+            output.append(reg_b % 8)
+        elif operand == 6:
+            output.append(reg_c % 8)
+    elif opcode == 6: # bdv
+        print(f"bdv: reg_b = reg_a // (2 ** operand)")
+        if operand < 4:
+            reg_b = reg_a // (2 ** operand)
+        elif operand == 4:
+            reg_b = reg_a // (2 ** reg_a)
+        elif operand == 5:
+            reg_b = reg_a // (2 ** reg_b)
+        elif operand == 6:
+            reg_b = reg_a // (2 ** reg_c)
+    elif opcode == 7: # cdv
+        print(f"cdv: reg_c = reg_a // (2 ** operand)")
+        if operand < 4:
+            reg_c = reg_a // (2 ** operand)
+        elif operand == 4:
+            reg_c = reg_a // (2 ** reg_a)
+        elif operand == 5:
+            reg_c = reg_a // (2 ** reg_b)
+        elif operand == 6:
+            reg_c = reg_a // (2 ** reg_c)
+
+print(",".join(map(str, output)))
+# Correct Answer: 7,3,0,5,7,1,4,0,5
